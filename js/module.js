8df135eb281160f7ea9d5357d408abc0468b7140
Matt Bullen <mattbullen@users.noreply.github.com>
2017-03-26 13:09:44 -0700

    Fix GitHub autoformatting

--- js/module.js
+++ js/module.js
@@ -1,22 +1,22 @@
 angular.module("charts", [])
-    .controller("mainController", function AppController ($scope) {
-		$scope.month = "Jan";
-		$scope.day = "1";
-		$scope.year = new Date().getFullYear();
-		$scope.hour = "12";
-		$scope.minutes = "01";
-		$scope.ampm = "PM";
-        $scope.hovered = function(d){
-			if (d.month) { $scope.month = d.month; }
-			if (d.day) { $scope.day = d.day; }
-			if (d.hour) { $scope.hour = d.hour; }
-			if (d.minutes) { $scope.minutes = d.minutes; }
-			if (d.ampm) { $scope.ampm = d.ampm; }
-			$scope.$apply();
+    .controller("mainController", function AppController($scope) {
+	$scope.month = "Jan";
+	$scope.day = "1";
+	$scope.year = new Date().getFullYear();
+	$scope.hour = "12";
+	$scope.minutes = "01";
+	$scope.ampm = "PM";
+        $scope.hovered = function(d) {
+		if (d.month) { $scope.month = d.month; }
+		if (d.day) { $scope.day = d.day; }
+		if (d.hour) { $scope.hour = d.hour; }
+		if (d.minutes) { $scope.minutes = d.minutes; }
+		if (d.ampm) { $scope.ampm = d.ampm; }
+		$scope.$apply();
         };
     })
-    .directive("chart", function(){
-		var chart = d3.custom.chart();
+    .directive("chart", function() {
+	var chart = d3.custom.chart();
         return {
             restrict: "E",
             replace: true,
@@ -26,7 +26,7 @@ angular.module("charts", [])
             },
             link: function(scope, element, attrs) {
                 var chartElement = d3.select(element[0]); 
-				chart.on("customMovement", function(d, i){
+		chart.on("customMovement", function(d, i){
                     scope.hovered({args:d}); 
                 });
                 scope.$watch("data", function (newVal, oldVal) {
